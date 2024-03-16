a6422bdc7cad197c1dd3db2845ecb9e8eb6dda40
Alekkivi <Aleksi.Kivilehto@metropolia.fi>
2024-03-16 20:50:40 +0200

    Apu

--- vite-project/regular.js
+++ vite-project/regular.js
@@ -716,19 +716,19 @@ async function postNewEntry(entry) {
   }
 }
 
-// async function postRequest(endpoint, postBody) {
-//   // Define request
-//   let token = localStorage.getItem("token");
-//   const options = {
-//     method: "POST",
-//     headers: {
-//       Authorization: "Bearer: " + token,
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(postBody),
-//   };
-//   return fetchData(endpoint, options)
-// }
+async function postRequest(endpoint, postBody) {
+  // Define request
+  let token = localStorage.getItem("token");
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: "Bearer: " + token,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(postBody),
+  };
+  return fetchData(endpoint, options)
+}
 
 
 
